78bcdb1f1f006e5fbe0fa3aca9eef924ccd92539
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-02-17 14:30:14 -0800

    work in progress

--- assignment2/array_search.c
+++ assignment2/array_search.c
@@ -10,8 +10,8 @@
 
 //Programming language: C language
 //Date development begun: 2024-Feb-10
-//Date of last update:    2024-Feb-12
-//Status: Print the file size
+//Date of last update:    2024-Feb-17
+//Status: copy all characters from file to an array
 //Compile:  gcc -c -Wall -m64 -no-pie -o search.o array_search.c -lm -std=c2x
 //Link:  gcc -m64 -no-pie -o search.out search.o -lm -std=c2x
 
@@ -22,54 +22,53 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 int main(int argc, char* argv[])
 {
   // Ask the user for input (name of txt file that we want to read)
   FILE *file; // Pointer to input file
-  //long int file_size; // calculate the size of the file
-  //int char_count = 0;
+ // long int file_size; // calculate the size of the file
+  int char_count;
   //char character;
   const unsigned int max_length_file_name = 64;
-  //char file_name[max_length_file_name];
+  char file_name[max_length_file_name];
   printf("%s", "Welcome to Great String Search brought to you by Sama Ahmed.\n");
-  //printf("Please enter the name of your file: %s", file_name);
+  //printf("Please enter the name of your file: ");
+  //scanf("%s", file_name);
 
   // open the file in read only mode
-  //file_name[strlen(file_name-1)] = '\0';
+  //file_name[strlen(file_name) - 1] = '\0';
   file = fopen("sheep.txt", "r"); // open the file for reading
+
+  // checking if the file exists, might want to do a do while loop, while the file == NULL, keep prompting the user to enter a filename
+  if (NULL == file) {
+    printf("%s", "Sorry. File was not found. Try again.\n");
+    printf("Please enter the name of your file: ");
+    scanf("%s", file_name);
+  } else {
+    printf("Test Status: found\n");
+  }
+
+  printf("The file has been opened with read permission.\n");
   fseek(file, 0L, SEEK_END);
   long fileLength = ftell(file); // gets the size of the file
-  printf("The size of this file is %lu.\n", fileLength);
-  char my_array[1000];
-  fgets(my_array, 1000, file);
-  printf("%s", my_array);
-  fclose(file);
+  fseek(file, 0L, SEEK_SET);
+  printf("The file contains %lu chars.\n", fileLength);
 
-  // checking if the file exists
-  //if (file == NULL) {
-    //printf("%s", "Sorry. File was not found. Try again.\n");
-    //printf("Please enter the name of your file: %s\n", file_name);
-  //} else {
-   // printf("found");
-  //}
+  char *arr;
+  arr = malloc(fileLength);
+  int c;
+  size_t n = 0;
+  while((c = fgetc(file)) != EOF) {
+    arr[n++] = (char)c;
+  }
+  arr[n] = '/0';
 
-  //printf("The file has been opened with read permission.\n");
+  for (int i = 0; i < strlen(arr); i++) {
+    printf("%c ", arr[i]);
+  }
 
-  // Compute the size of the file in bytes
-  //file_size = ftell(file);
-  // For testing purposes, print to see if the size of the file is printed.
-  //printf("Size of this file is %ld", file_size);
-
-  //for (character = getc(file); character != EOF; character = getc(file)) { // scanning through the file so that  can track char count
-   // char_count += 1;
-  //}
-  //printf("The file contains %i chars", char_count);
-  //printf("The file has been copied to a one-dimensional array.\n");
-
-
-  // Declare an array of char that us the same size as the file size
-
-  // copy all data from the file to the array of chars
+    printf("The file has been copied to a one-dimensional array.\n");
 
   // ask the user to enter a short string to search for
 
@@ -79,6 +78,7 @@ int main(int argc, char* argv[])
 
   // If user enters an empty string or if the string is not present, then print that the string was not found.
 
-
- return 0;
+  printf("Have a good day. We hope you enjoyed the string search.\n");
+  printf("Come back any time for more searching. Bye\n");
+  return 0;
 }//End of main
