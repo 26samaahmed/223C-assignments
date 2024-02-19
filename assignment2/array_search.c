54a6040b83f4e172295ab247b8fee08802a21267
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-02-19 13:26:40 -0800

    work in progress

--- assignment2/array_search.c
+++ assignment2/array_search.c
@@ -10,8 +10,8 @@
 
 //Programming language: C language
 //Date development begun: 2024-Feb-10
-//Date of last update:    2024-Feb-18
-//Status: Ask user and return every occurence of the first letter from the input
+//Date of last update:    2024-Feb-19
+//Status: Loop until the the user control d. The positions are only printed for the first input only.
 //Compile:  gcc -c -Wall -m64 -no-pie -o search.o array_search.c -lm -std=c2x
 //Link:  gcc -m64 -no-pie -o search.out search.o -lm -std=c2x
 
@@ -42,30 +42,31 @@ int main(int argc, char* argv[])
   file = fopen("sheep.txt", "r"); // open the file for reading
 
   //checking if the file exists, might want to do a do while loop, while the file == NULL, keep prompting the user to enter a filename
-  if (NULL == file) {
+  while (NULL == file) {
     printf("%s", "Sorry. File was not found. Try again.\n");
-   // printf("Please enter the name of your file: ");
-   // scanf("%s", file_name);
-  } else {
+    printf("Please enter the name of your file: ");
+    //scanf("%s", file_name);
+  }
+  if (file != NULL) {
 
-    printf("The file has been opened with read permission.\n");
+    printf("\nThe file has been opened with read permission.\n");
     fseek(file, 0L, SEEK_END);
     long fileLength = ftell(file); // gets the size of the file
     printf("The file contains %lu chars.\n", fileLength);
     rewind(file);
 
     char *arr;
-    arr = malloc(fileLength);
+    arr = malloc(fileLength);           // dynamically allocating memory with the same size as the file
     int c;
     size_t n = 0;
-    while((c = fgetc(file)) != EOF) {
-      arr[n++] = (char)c;
+    while((c = fgetc(file)) != EOF) {   // as long as we haven't reached the end of file, keep looping
+      arr[n++] = (char)c;   // store each character associated with each index
     }
     rewind(file);
   // arr[n] = "/0";
 
-    for (int i = 0; i < strlen(arr); i++) {
-      printf("%c", arr[i]);
+    for (int i = 0; i < strlen(arr); i++) {   // looping through the array of characters
+      printf("%c", arr[i]);                   // printing the character at that index
     }
 
     printf("\nThe file has been copied to a one-dimensional array.\n");
@@ -74,28 +75,43 @@ int main(int argc, char* argv[])
     // ask the user to enter a short string to search for
     char input[max_search_length];
     int i = 0;
-    int location[fileLength];
+    int location[fileLength];   // initializing an array of integers with the same size as the file
     int loc = 0;
-    //while (!feof(stdin)) {
-    printf("Enter a string to find or enter cntl+d to terminate: ");
-    scanf("%s", input);
-    while (i < strlen(arr) - strlen(input)) {
-      if (strncmp(arr+i, input, strlen(input)) == 0) {
-        location[loc] = i;
-        loc++;
+    while (!feof(stdin)) {
+      printf("\nEnter a string to find or enter cntl+d to terminate: ");
+      scanf("%s", input);
+      printf("%s was found at positions ", input);
+      while (i < strlen(arr) - strlen(input)) {     // as long as we still have characters left
+
+        // comparing the the length of the array and length of input. The maximum number of
+        // characters to be compared is the length of the input. If it's equal to zero, then it
+        // indicates we found a match since they're equal
+        if (strncmp(arr+i, input, strlen(input)) == 0) {
+          location[loc] = i;  // store the position of the found character in the array of positions
+          printf("%d ", i + 2);
+          loc++;
+        }
+        i++;
       }
-      i++;
+      printf("%s", "\n");
     }
 
- //}
+    // looping through the array of positions to print. Not sure why I'm moving 2 indices ahead yet but it gives the right input
+    // int j= 0;
+    // for (j = 0; j < loc; j++) {
+    //   printf("%d ", location[j] + 2);
+    // }
+  //}
 
-    int j= 0;
-    for (j = 0; j < loc; j++) {
-      printf("%s was found at positions %d\n\n", input, location[j] + 2);
-    }
-
+    printf("%s", "\n");
+    printf("Thank you for using this search program.\n");
     printf("Have a good day. We hope you enjoyed the string search.\n");
     printf("Come back any time for more searching. Bye\n");
   }
   return 0;
 }//End of main
+
+
+// TODO: fix the file path thing where i get to enter the file name and the program would then check if it exists
+// TODO: do a while loop that keeps prompting the user until they do control d
+// TODO: print position is found at none when the substring doesn't exist
