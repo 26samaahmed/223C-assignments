6ceea9295055913dfc1fe043b0ce41a24e49e25f
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-02-20 23:22:28 -0800

    Update array_search.c

--- assignment2/array_search.c
+++ assignment2/array_search.c
@@ -10,8 +10,8 @@
 
 //Programming language: C language
 //Date development begun: 2024-Feb-10
-//Date of last update:    2024-Feb-19
-//Status: Loop until the the user control d. The positions are only printed for the first input only.
+//Date of last update:    2024-Feb-20
+//Status: Ask user to enter a string to search for until they enter control d
 //Compile:  gcc -c -Wall -m64 -no-pie -o search.o array_search.c -lm -std=c2x
 //Link:  gcc -m64 -no-pie -o search.out search.o -lm -std=c2x
 
@@ -42,13 +42,13 @@ int main(int argc, char* argv[])
   file = fopen("sheep.txt", "r"); // open the file for reading
 
   //checking if the file exists, might want to do a do while loop, while the file == NULL, keep prompting the user to enter a filename
-  while (NULL == file) {
-    printf("%s", "Sorry. File was not found. Try again.\n");
-    printf("Please enter the name of your file: ");
-    //scanf("%s", file_name);
-  }
-  if (file != NULL) {
+  // while (NULL == file) {
+  //   printf("%s", "Sorry. File was not found. Try again.\n");
+  //   printf("Please enter the name of your file: ");
+  //   //scanf("%s", file_name);
+  // }
 
+  if (file != NULL) {
     printf("\nThe file has been opened with read permission.\n");
     fseek(file, 0L, SEEK_END);
     long fileLength = ftell(file); // gets the size of the file
@@ -63,8 +63,9 @@ int main(int argc, char* argv[])
       arr[n++] = (char)c;   // store each character associated with each index
     }
     rewind(file);
-  // arr[n] = "/0";
+    arr[n] = '\0';
 
+    // Testing Purposes
     for (int i = 0; i < strlen(arr); i++) {   // looping through the array of characters
       printf("%c", arr[i]);                   // printing the character at that index
     }
@@ -73,19 +74,18 @@ int main(int argc, char* argv[])
 
 
     // ask the user to enter a short string to search for
-    char input[max_search_length];
-    int i = 0;
-    int location[fileLength];   // initializing an array of integers with the same size as the file
-    int loc = 0;
     while (!feof(stdin)) {
+      char input[max_search_length];
+      int i = 0;
+      int location[fileLength]; // initializing an array of integers with the same size as the file
+      int loc = 0;
       printf("\nEnter a string to find or enter cntl+d to terminate: ");
       scanf("%s", input);
       printf("%s was found at positions ", input);
+      if (input == ' ' || strncmp(arr+i, input, strlen(input)) != 0) {
+        printf("%s ", "none");
+      }
       while (i < strlen(arr) - strlen(input)) {     // as long as we still have characters left
-
-        // comparing the the length of the array and length of input. The maximum number of
-        // characters to be compared is the length of the input. If it's equal to zero, then it
-        // indicates we found a match since they're equal
         if (strncmp(arr+i, input, strlen(input)) == 0) {
           location[loc] = i;  // store the position of the found character in the array of positions
           printf("%d ", i + 2);
@@ -96,22 +96,11 @@ int main(int argc, char* argv[])
       printf("%s", "\n");
     }
 
-    // looping through the array of positions to print. Not sure why I'm moving 2 indices ahead yet but it gives the right input
-    // int j= 0;
-    // for (j = 0; j < loc; j++) {
-    //   printf("%d ", location[j] + 2);
-    // }
-  //}
-
     printf("%s", "\n");
     printf("Thank you for using this search program.\n");
     printf("Have a good day. We hope you enjoyed the string search.\n");
     printf("Come back any time for more searching. Bye\n");
+    fclose(file);
   }
   return 0;
 }//End of main
-
-
-// TODO: fix the file path thing where i get to enter the file name and the program would then check if it exists
-// TODO: do a while loop that keeps prompting the user until they do control d
-// TODO: print position is found at none when the substring doesn't exist
