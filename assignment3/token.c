851495b0b7c1e4e76c26819c4e5e9f8f374608be
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-03-05 22:25:18 -0800

    Update token.c

--- assignment3/token.c
+++ assignment3/token.c
@@ -1,7 +1,7 @@
 // Author: Sama Ahmed
 // Contact: 26samahmed@csu.fullerton.edu
 // Course ID: CPSC223C
-// Due Date: 2024-Mar-14 at 2:00 AM
+// Due Date: 2024-Mar-13 at 11:59
 // Program name: Read and Output File
 
 // Purpose
@@ -11,7 +11,7 @@
 // Programming language: C language
 // Date development begun: 2024-Mar-4
 // Date of last update:    2024-Mar-4
-// Status: Working on the token part
+// Status: Implemented the strrok part
 // Compile:  gcc -c -Wall -m64 -no-pie -o search.o array_search.c -lm -std=c2x
 // Link:  gcc -m64 -no-pie -o search.out search.o -lm -std=c2x
 
@@ -30,39 +30,45 @@ int main(int argc, char *argv[])
   char file_name[max_length_file_name];
   char input[max_search_length];
   char * token;
+  int token_count = 0;
 
   printf("%s", "Welcome to Tokenizer brought to you by Sama Ahmed\n\n");
   printf("Please enter the name of your file: ");
   scanf("%s", file_name);
 
   file = fopen(file_name, "r"); // open the file for reading
+  file_name[strlen(file_name) - 1] = '\0';
 
   // Prompt the user to enter a valid file name that's in the directory
-  while (file == NULL)
-  {
+  while (file == NULL) {
     printf("%s", "Sorry. File was not found. Try again.\n");
     printf("Please enter the name of your file: ");
     scanf("%s", file_name);
     file = fopen(file_name, "r"); // open the file for reading
   }
 
+
   // If the file exists then ask the user for their input to search
   if (file != NULL) {
     printf("\nThe file has been opened with read permission.\n");
     fseek(file, 0L, SEEK_END);
     long fileLength = ftell(file); // gets the size of the file
-    printf("The file contains %lu chars.\n", fileLength);
+    printf("The file contains %lu chars.\n", fileLength + 1);
     rewind(file);
 
     char *arr;
     arr = malloc(fileLength); // dynamically allocating memory with the same size as the file
     int character;
     size_t counter = 0;
-    while ((character = fgetc(file)) != EOF)
-    {                     // as long as we haven't reached the end of file, keep looping
-      arr[counter++] = (char)character; // store each character associated with each index
+    while ((character = fgetc(file)) != EOF) {      // as long as we haven't reached the end of file, keep looping
+      // TODO: replace any newlines found with a space
+      if (character == '\n') {
+        putchar(' ');
+      }else {
+        arr[counter++] = (char)character;             // store each character associated with each index
+      }
     }
-    // TODO: replace any newlines found with a space
+
     rewind(file);
 
     printf("The file has been copied to a one-dimensional array and all newlines have been replace by the space character.\n");
@@ -74,12 +80,35 @@ int main(int argc, char *argv[])
     printf("%s", "\nEnter some characters for the separator string and press enter: ");
     scanf("%s", input);
 
-    // get the first token
+    // Get the first token.
+    // The first parameter is the array of chars to be tokenized and the second parameters contains the separators
     token = strtok(arr, input);
-    while (token != NULL) { // check the rest of the tokens
-      printf("%s\n", token);
-      token = strtok(NULL, input);
+
+    if (token != NULL) {
+      printf("%s", "The tokens were found and placed into a two-dimensional array of char.  Here are the contents of the token array in forward order.\n\n");
     }
+
+    // char * token_arr[fileLength]; // create an array that can handle up to the same size as if the separators were there
+    // size_t j = 0;
+
+    while (token != NULL) {               // check the rest of the tokens
+      printf("%s\n", token);
+      token_count += 1;
+      token = strtok(NULL, input);
+     // token_arr[j++] = token;
+
+      // TODO:Place the tokens in a two dimensional array of char
+    }
+
+    // for (unsigned int i = 0; i < strlen(token_arr); i++) {
+    //   printf("%c", token_arr[i]);
+    // }
+
+    printf("\nNumber of tokens is %d", token_count);
+    printf("%s", "\n");
+
+    // TODO: write the array of tokens so same current file name but with the extension .tok which is a token file
+    // TODO: Get the size of the token file
 
     fclose(file);
   }
